cd9f549a508617ffb02edc6580aa66536886bf0e
Daan Rongen <daanrongen@hotmail.com>
2022-08-02 22:21:19 +0100

    refactor to opencv

--- prepare-dataset.py
+++ prepare-dataset.py
@@ -1,45 +1,36 @@
 import glob
-from PIL import Image
+import cv2
 import argparse
 import os
+from skimage.util import random_noise
+import numpy as np
 from tqdm import tqdm
 
-from utils import remove_frames, center_crop, flip
-
-# def crop_center(pil_img, crop_width, crop_height):
-#     img_width, img_height = pil_img.size
-#     return pil_img.crop(((img_width - crop_width) // 2,
-#                          (img_height - crop_height) // 2,
-#                          (img_width + crop_width) // 2,
-#                          (img_height + crop_height) // 2))
-
-# def crop_max_square(pil_img):
-#     return crop_center(pil_img, min(pil_img.size), min(pil_img.size))
 
 def main(args):
     images = glob.glob(f"{args.indir}/*.*")
     os.makedirs(args.outdir, exist_ok=True)
 
     for i, infile in tqdm(enumerate(images)):
-        image = (
-            Image.open(infile)
-            .convert("RGB")
-            .resize((args.size, args.size), Image.LANCZOS)
-        )
+        image = cv2.imread(infile)
+        print(image.shape)
 
-        # image = crop_max_square(image).resize((args.size, args.size), Image.LANCZOS)
-        image = remove_frames(image)
-        image = center_crop(image, args.size)
+        image = cv2.resize(image, (args.size, args.size), cv2.INTER_CUBIC)
+        print(image.shape)
 
-        assert image.size == (args.size, args.size)
-        assert image.mode == "RGB"
-        image.save(f"{args.outdir}/{i:06d}.png", "PNG", quality=100)
+        cv2.imwrite(f"{args.outdir}/{i:06d}-0.png", image)
 
-        if args.flip:
-            flipped = flip(image)
-            assert flipped.size == (args.size, args.size)
-            assert flipped.mode == "RGB"
-            flipped.save(f"{args.outdir}/{i:06d}-flip.png", "PNG", quality=100)
+        if (args.invert):
+            cv2.imwrite(f"{args.outdir}/{i:06d}-clr.png", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+
+        if (args.noise):
+            noise = random_noise(image)
+            noise = np.array(255 * noise, dtype=np.uint8)
+            cv2.imwrite(f"{args.outdir}/{i:06d}-nse.png", noise)
+
+        if (args.blur):
+            blur_image = cv2.GaussianBlur(image, (11, 11), 0)
+            cv2.imwrite(f"{args.outdir}/{i:06d}-blr.png", blur_image)
 
 
 if __name__ == "__main__":
@@ -47,7 +38,9 @@ if __name__ == "__main__":
     parser.add_argument("--size", type=int, default=1024)
     parser.add_argument("--indir", type=str, default="/content/drive/MyDrive/data/gan/images/in")
     parser.add_argument("--outdir", type=str, default="/content/drive/MyDrive/data/gan/images/out")
-    parser.add_argument("--flip", type=bool, default=False)
+    parser.add_argument("--invert", type=bool, default=False)
+    parser.add_argument("--blur", type=bool, default=False)
+    parser.add_argument("--noise", type=bool, default=False)
     args = parser.parse_args()
 
     main(args)
